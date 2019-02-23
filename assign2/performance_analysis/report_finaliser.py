95173d4f3404589832154be9cd3686fd238102ff
vismay <JarYamsiv>
2019-02-23 12:55:05 +0530

    added graph making py script. busy-wait too slow

--- assign2/performance_analysis/report_finaliser.py
+++ assign2/performance_analysis/report_finaliser.py
@@ -1,3 +1,57 @@
 import matplotlib.pyplot as plt
+import sys
 
-print("making graphs ")
+file_name = sys.argv[1]
+
+print("making graphs for "+file_name)
+
+
+
+mrf = open("report/"+file_name+".txt","r")
+
+
+
+contents = mrf.readlines()
+
+mutex_report_dict = {}
+counts = {}
+t_counts = {}
+time_taken = 0
+count = 0
+thread_count = 0
+
+for i in range(len(contents)):
+	splited = contents[i].split(" ")
+
+	if i%3 == 1:
+		count = int(splited[1])
+		counts[count] = 1
+		thread_count = int(splited[2])
+		t_counts[thread_count] = 1
+
+	if i%3 == 2:
+		time_taken = float(splited[0])
+		mutex_report_dict[(count,thread_count)] = time_taken
+		
+
+count_key = list(counts.keys())
+t_keys = list(t_counts.keys())
+
+#print(count_key)
+#print(t_keys)
+
+colors = [ 'b' , 'g' , 'r' , 'c' , 'm', 'y', 'k']
+
+plt.ylabel("time takes (s)")
+plt.xlabel("number of threads")
+for (i,c) in enumerate(count_key):
+	t_array = []
+	s_array = []
+	for t in t_keys:	
+		t_array.append(t)
+		s_array.append(mutex_report_dict[(c,t)])
+
+	plt.plot(t_array,s_array,colors[i],label="count="+str(c))
+plt.legend()
+plt.savefig('report/final/'+file_name+'.png', dpi=500)
+		
--- assign2/performance_analysis/report_maker.sh
+++ assign2/performance_analysis/report_maker.sh
@@ -1,8 +1,10 @@
 echo "making report please wait.."
 
-COUNT_MAX_ARRAY=(1000 10000 100000 1000000 10000000)
+COUNT_MAX_ARRAY=(1000 10000 100000 1000000)
 
-THREAD_ARRAY=(1 2 4 10 20 50 100 200)
+THREAD_ARRAY=(1 2 3 4)
+
+PROGRAMS=(mutex busy_wait)
 
 THREAD_ARRAY_SIZE=${#THREAD_ARRAY[@]}
 COUNT_ARRAY_SIZE=${#COUNT_MAX_ARRAY[@]}
@@ -10,27 +12,39 @@ COUNT_ARRAY_SIZE=${#COUNT_MAX_ARRAY[@]}
 
 TOTAL_ARRAY_SIZE=$[${THREAD_ARRAY_SIZE}*${COUNT_ARRAY_SIZE}]
 
-COUNTER=0
 
-printf "running mutex with %d cases\n" ${TOTAL_ARRAY_SIZE}
-echo "" > report/mutex.txt
-for c in ${COUNT_MAX_ARRAY[*]}
+
+
+for p in ${PROGRAMS[*]}
 do
-	for t in ${THREAD_ARRAY[*]}
+	printf "running %s with %d cases\n" ${p} ${TOTAL_ARRAY_SIZE}
+	COUNTER=0
+	echo "" > report/${p}.txt
+	for c in ${COUNT_MAX_ARRAY[*]}
 	do
-		COUNTER=$[$COUNTER+1]
+		for t in ${THREAD_ARRAY[*]}
+		do
+			COUNTER=$[$COUNTER+1]
 
-		printf "\r%d of %d done" ${COUNTER} ${TOTAL_ARRAY_SIZE}
+			printf "\r%d of %d done" ${COUNTER} ${TOTAL_ARRAY_SIZE}
 
-		echo "mut" ${c} ${t} >> report/mutex.txt
-		./mutex/main ${c} ${t} >> report/mutex.txt
-		echo "" >> report/mutex.txt
+			echo "mut" ${c} ${t} >> report/${p}.txt
+			./${p}/main ${c} ${t} >> report/${p}.txt
+			echo "" >> report/${p}.txt
+		done
 	done
+	printf "\n"
 done
-printf "\n"
 
 
-python report_finaliser.py
+for p in ${PROGRAMS[*]}
+do
+	python report_finaliser.py ${p}
+done
 
+lscpu > report/hardware_spec.txt
 printf "report made \n"
+
+
+
 
