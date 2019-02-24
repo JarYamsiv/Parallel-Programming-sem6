5beaec2febb938eb90a2d5b8e4090cd7b9f34232
vismay <JarYamsiv>
2019-02-24 11:02:46 +0530

    added terminal clear functionality

--- assign2/performance_analysis/README.md
+++ assign2/performance_analysis/README.md
@@ -1,10 +1,26 @@
 # Performance Analysis of Synchronisation constructs
 
+## Prerequesties
+		For fully functioning it is required to have lpthread library for c and
+		matplotlib library for python to be installed
+		and also to execute the bash file make sure you do
+```console
+chmod +x report_maker.sh
+```
+			 before running 
+```console
+make run
+```
+
 ### Create the graphs and hardware spec details using
-		make run	
+```console
+make run	
+```
 
 ### Clean the unwanted file using
-		make clean
+```console
+make clean
+```
 		
 ## Report
     The Analysis and reasons for observations in my computer is in the pdf report/final/my-report.pdf
--- assign3/kilo/kilo.c
+++ assign3/kilo/kilo.c
@@ -1,33 +1,91 @@
+/**** includes ****/
+
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
+#include <errno.h>
 
-struct termios orig_termios;
+/**** defines ****/
+#define CTRL_KEY(k) ((k) & 0x1f)
+
+
+/**** data ****/
+struct termios orig_termios; //the original one which later can be used 
+
+
+
+/**** function definitions ****/
+
+/**** terminal ****/
+void die(const char *s) {
+  perror(s);
+  exit(1);
+}
 
 void disableRawMode() {
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+  if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios)==-1){
+    die("tcsetattr");
+  }
 }
 
 void enableRawMode() {
-  tcgetattr(STDIN_FILENO, &orig_termios);
+
+  if(tcgetattr(STDIN_FILENO, &orig_termios)==-1)die("tcgetattr");
   atexit(disableRawMode);
-  struct termios raw = orig_termios;
-  raw.c_iflag &= ~(IXON);
-  raw.c_lflag &= ~(ECHO | ICANON | ISIG);
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+
+  struct termios raw = orig_termios; //to set the raw attributes
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON); //fixes ctrl+m , disable ctrl+s , ctrl+q 
+  raw.c_oflag &= ~(OPOST); //turns off o/p processing now we need to use /r while printing
+  raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN); // turns off echo , cononical mode(now it reads byte by byte instead of line) , ctrl+c , ctrl+z
+                                                   //disables  ctrl+v
+
+  raw.c_cc[VMIN] = 0; //time out
+  raw.c_cc[VTIME] = 10;
+
+  if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw)==-1)die("tcsetattr");
+  //setting the attributes previuosly defined
 }
 
-int main() {
-  enableRawMode();
+/**** input processing ****/
+char editorReadKey() {
+  int nread;
   char c;
-   while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
-    if (iscntrl(c)) {
-      printf("%d\n", c);
-    } else {
-      printf("%d ('%c')\n", c, c);
-    }
+  while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+    if (nread == -1 && errno != EAGAIN) die("read");
+  }
+  return c;
+}
+
+void editorProcessKeypress() {
+  char c = editorReadKey();
+  switch (c) {
+    case CTRL_KEY('q'):
+      exit(0);
+      break;
+  }
+}
+
+
+/**** output processing ****/
+void editorRefreshScreen() {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  //clears screen
+}
+
+
+/**** function def end ****/
+
+/**** main function ****/
+int main() {
+
+  enableRawMode();
+
+   while (1) {
+    editorRefreshScreen();
+    editorProcessKeypress();
+
   }
   disableRawMode();
   return 0;
